af33862011ecd401896049d124907ffe81308e1a
Guilherme Vanderley <guivdouradog@gmail.com>
2022-10-18 19:05:05 -0300

    init login page add logo, create input components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,26 @@
 import Head from "next/head"
+import Image from "next/image"
+import logoImg from '../../public/logo.svg'
+import styles from '../../styles/home.module.scss'
+import { Input } from '../components/ui/Input'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>MY KITCHEN</title>
+        <title>My Kitchen</title>
       </Head>
 
-      <div>
-        <h1>Bem vindo ao MyKitchen</h1>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt='Logo' />
+
+        <div className={styles.login}>
+          <form>
+            <Input placeholder="Digite seu email:" type='text'/>
+
+            <Input placeholder="Digite sua senha:" type='password'/>
+          </form>
+        </div>
       </div>
     </>
   )
