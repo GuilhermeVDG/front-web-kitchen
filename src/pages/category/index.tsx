69e92382afff44130700445bad28c8e4a1252634
Guilherme Vanderley <guivdouradog@gmail.com>
2022-10-29 17:11:31 -0300

    add category to db

--- src/pages/category/index.tsx
+++ src/pages/category/index.tsx
@@ -1,8 +1,31 @@
+import { useState, FormEvent } from 'react';
 import Head from "next/head";
 import { Header } from '../../components/Header';
 import styles from './styles.module.scss';
+import { api } from '../../services/apiClient';
+import { toast } from 'react-toastify'; 
 
 export default function Category(){
+  const [ category, setCategory ] = useState('');
+
+  async function handleRegister(e: FormEvent){
+    e.preventDefault();
+
+    if(category === ''){
+      toast.error('Preencha o nome da catrgoria');
+      return;
+    }
+
+    await api.post('/category', {
+      name: category
+    });
+
+
+
+    toast.success('Categoria cadastrada com sucesso');
+    setCategory('');
+  }
+  
   return(
     <>
       <Head>
@@ -14,11 +37,13 @@ export default function Category(){
         <main className={styles.container}>
           <h1>Cadastrar nova categoria</h1>
 
-          <form className={styles.form}>
+          <form className={styles.form} onSubmit={handleRegister}>
             <input
             type='text'
             placeholder='Digite o nome da nova categoria'
             className={styles.input}
+            value={category}
+            onChange={e => setCategory(e.target.value)}
             />
 
             <button type="submit" className={styles.buttonSubmit}>Cadastrar</button>
