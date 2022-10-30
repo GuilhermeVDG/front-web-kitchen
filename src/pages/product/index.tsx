8d601bee8a34d185904756c8fffe53ffbe9d9a3f
Guilherme Vanderley <guivdouradog@gmail.com>
2022-10-29 23:57:54 -0300

    create preview

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,3 +1,4 @@
+import { useState, ChangeEvent } from 'react';
 import Head from "next/head";
 import styles from './styles.module.scss';
 import { Header } from "../../components/Header";
@@ -5,6 +6,22 @@ import { canSSRAuth } from "../../utils/canSSRAuth";
 import { FiUpload } from "react-icons/fi";
 
 export default function Product(){
+  const [imageAvatar, setImageAvatar] = useState(null);
+  const [avatarUrl, setAvatarUrl] = useState('');
+
+  function handleAvatar(event: ChangeEvent<HTMLInputElement>){
+    if(!event.target.files) return;
+
+    const image = event.target.files[0];
+
+    if(!image) return;
+
+    if(image.type === 'image/png' || image.type === 'image.jpeg'){
+      setImageAvatar(image);
+      setAvatarUrl(URL.createObjectURL(image));
+    }
+  }
+  
   return(
     <>
       <Head>
@@ -23,7 +40,17 @@ export default function Product(){
                 <FiUpload size={25} color='#fff'/>
               </span>
 
-              <input type='file' accept='image/png, image/jpeg'/>
+              <input type='file' accept='image/png, image/jpeg' onChange={handleAvatar}/>
+
+              {avatarUrl && (
+                <img
+                src={avatarUrl}
+                alt="Imagem do produto"
+                height={250}
+                width={250}
+                className={styles.preview}
+              />
+              )}
             </label>
             
             <select>
