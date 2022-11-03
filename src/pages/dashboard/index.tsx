dbea91ba64113507ab6a71249c87eaf665638526
Guilherme Vanderley <guivdouradog@gmail.com>
2022-11-02 21:50:35 -0300

    create list orders

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,10 +1,27 @@
+import { useState } from 'react';
 import { canSSRAuth } from "../../utils/canSSRAuth"
 import Head from "next/head"
 import { Header } from "../../components/Header"
 import styles from './styles.module.scss';
 import { FiRefreshCcw } from "react-icons/fi";
+import { setupAPIClient } from "../../services/api";
 
-export default function Dashboard(){
+type OrderProps = {
+  id: string;
+  table: string | number;
+  status: boolean;
+  draft: boolean;
+  name: string | null
+}
+
+interface HomeProps{
+  listOrders: OrderProps[];
+}
+
+export default function Dashboard({ listOrders }: HomeProps){
+  const [orderList, setOrderList] = useState(listOrders || []);
+  
+  
   return(
     <>
     <Head>
@@ -24,12 +41,14 @@ export default function Dashboard(){
 
       <article className={styles.listOrders}>
         
-        <section className={styles.orderItem}>
-          <button>
-            <div className={styles.tag}></div>
-            <span>Mesa tal</span>
-          </button>
-        </section>
+        {orderList.map(item => (
+          <section key={item.id} className={styles.orderItem}>
+            <button>
+              <div className={styles.tag}></div>
+              <span>Mesa {item.table}</span>
+            </button>
+          </section>
+        ))}
       
       </article>
     </main>
@@ -40,9 +59,14 @@ export default function Dashboard(){
 }
 
 export const getServerSideProps = canSSRAuth(async ctx => {
+  const api = setupAPIClient(ctx);
+
+  const response = await api.get('/order/list');
+  
+  
   return {
     props: {
-      
+      listOrders: response.data
     }
   }
 })
